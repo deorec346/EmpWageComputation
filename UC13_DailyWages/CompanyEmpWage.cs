189cca7b9728806caf214ab292e916a25a14b4b7
[chetan deore] <[deorec346@gmail.com]>
2022-04-16 00:07:13 +0530

    UC13_DailyWages

---
+++ UC13_DailyWages/CompanyEmpWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC13_DailyWages
+{
+    internal class CompanyEmpWage
+    {
+            public string company;
+            public int emp_Rate_Per_Hr, no_Of_Working_Days, max_Hrs_Per_Month, total_Emp_Wage;
+
+            public CompanyEmpWage(string company, int emp_Rate_Per_Hr, int no_Of_Working_Days, int max_Hrs_Per_Month)
+            {
+                this.company = company;
+                this.emp_Rate_Per_Hr = emp_Rate_Per_Hr;
+                this.no_Of_Working_Days = no_Of_Working_Days;
+                this.max_Hrs_Per_Month = max_Hrs_Per_Month;
+                this.total_Emp_Wage = 0;
+            }
+
+            public void setTotalEmpWage(int total_Emp_Wage)
+            {
+                this.total_Emp_Wage = total_Emp_Wage;
+            }
+
+            public string ToString()
+            {
+                return "Total Emp Wage For Company " + this.company + " is " + this.total_Emp_Wage;
+            }
+        
+
+    }
+}
---
+++ UC13_DailyWages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC13_DailyWages
+{
+    public class program
+    {
+            public static void Main(string[] args)
+            {
+                EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+                empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+                empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+                empWageBuilder.computeEmpWage();
+                Console.WriteLine("Total Emp Wage for Dmart Company :" + empWageBuilder.getTotalWage("Dmart"));
+                Console.WriteLine("Total Emp Wage for Reliance Company :" + empWageBuilder.getTotalWage("Reliance"));
+
+
+            }
+        
+    }
+}
