70489609629854831d75bab21b80b0e29a4e6b2f
[chetan deore] <[deorec346@gmail.com]>
2022-04-16 00:05:26 +0530

    UC12_List_Of_Multiple_Companies

---
+++ UC12_List_Of_Multiple_Companies/IComputeEmpWage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC12_List_Of_Multiple_Companies
+{
+        public interface IComputeEmpWage
+        {
+            public void addCompanyEmpWage(string companyName, int employeeRatePerHr, int maxWorkingDays, int maxWorkingHrs);
+            public void computeEmpWage();
+
+        }
+    
+}
+
---
+++ UC12_List_Of_Multiple_Companies/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC12_List_Of_Multiple_Companies
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Computation");
+            //creating object for each company
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("TVSNEXT", 20, 10, 10);
+            empWageBuilder.addCompanyEmpWage("HCL", 20, 10, 10);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
